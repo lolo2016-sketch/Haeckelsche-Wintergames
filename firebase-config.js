6d229a0b76abce2536f0267f56a0e729a5eb5a79
lolo2016-sketch <lorenzneukirch.ln@gmail.com>
2026-07-27 12:13:20 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDv-4Z8VPu05AJqGgxhVLrVEhIeSjeRzAM",
+  authDomain: "haeckelsche.firebaseapp.com",
+  projectId: "haeckelsche",
+  storageBucket: "haeckelsche.firebasestorage.app",
+  messagingSenderId: "887244302824",
+  appId: "1:887244302824:web:18c2f981291effdb269c5d",
+  measurementId: "G-3P9LMF51TN"
+};
